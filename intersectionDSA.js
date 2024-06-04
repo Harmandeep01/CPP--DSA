5379f67ca8e84edd76c12ad8b581f0299bbdde8e
Harmandeep <111681542+Harmandeep01@users.noreply.github.com>
2024-06-04 11:29:01 +0530

    Create intersectionDSA.js
    intersection with DSA

---
+++ intersectionDSA.js
@@ -0,0 +1,13 @@
+// Code example
+const arr1 = [1, 2, 3, 4 ];
+const arr2 = [1, 2, 0, 6];
+const arr3 = [];
+
+for (i in arr1) {
+    for (j in arr2) {
+        if (i === j){
+            
+        }
+    }
+
+console.log(arr3)
