ca8e65fad36e2897cff34fd8c05f1e0c9ddcacb0
Harmandeep <111681542+Harmandeep01@users.noreply.github.com>
2025-06-15 13:25:08 +0000

    Optimised Longest Consecutive

--- SDE/DAY4/longestConsecutive.cpp
+++ SDE/DAY4/longestConsecutive.cpp
@@ -29,6 +29,7 @@ int longestConsecutive(vector<int>& nums){
   return cnt;
 }
 */
+/*
 
 //Better Solution
 int longestConsecutive(vector<int>& nums){
@@ -50,9 +51,35 @@ int longestConsecutive(vector<int>& nums){
   }
   return longest;
 }
+*/
+
+//Optimal Solution
+int longestConsecutive(vector<int>& nums){
+  if(nums.size() == 0) return 0;
+  
+  int longest = 1;
+  int n = nums.size();
+  unordered_set<int> st;
+  for(int i = 0; i < n; i++){
+    st.insert(nums[i]);
+  }
+  
+  for(auto it : st){
+    if(st.find(it - 1) == st.end()){
+      int cnt = 1;
+      int x = it;
+      while(st.find(x + 1) != st.end()){
+        cnt++;
+        x = x + 1;
+      }
+      longest = max(longest, cnt);
+    }
+  }
+  return longest;
+}
 
 int main() {
-  vector<int> nums = {100,4,200,1,3,2};
+  vector<int> nums = {};
   int ans = longestConsecutive(nums);
   cout << ans;
 }
